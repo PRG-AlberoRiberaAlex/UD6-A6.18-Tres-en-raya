27d89ffbb42b6538e596c19fcfdfa24a8729ce5f
Alex_Albero <alberoriberaalex@gmail.com>
2024-03-07 17:37:02 +0100

    Teclado añadido

--- Jugador.java
+++ Jugador.java
@@ -1,10 +1,21 @@
 public class Jugador {
 
     private EstadoCasilla color;
-    public Jugador(EstadoCasilla color){
-        this.color=color;
+
+    public Jugador(EstadoCasilla color) {
+        this.color = color;
     }
-    public void ponerFicha(Tablero tablero){
-        System.out.print("Introduce fila");
+
+    public void ponerFicha(Tablero tablero) {
+        int num;
+        do {
+            System.out.print("Introduce fila[1-3]: ");
+            while (!TresEnRaya.teclat.hasNextInt()) {
+                System.out.print("Introduce fila[1-3]: ");
+                TresEnRaya.teclat.next();
+            }
+            num = TresEnRaya.teclat.nextInt();
+        } while (num <= 0);
     }
 }
+
--- TresEnRaya.java
+++ TresEnRaya.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
 public class TresEnRaya {
+    public static Scanner teclat = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner teclat = new Scanner(System.in);
         Tablero tablero=new Tablero();
         Jugador jugador1 = new Jugador(EstadoCasilla.FICHA_X);
         Jugador jugador2 = new Jugador(EstadoCasilla.Ficha_O);
         tablero.vaciar();
         tablero.mostrar();
+        jugador1.ponerFicha(tablero);
         if (tablero.estaLleno()){
             System.out.println("La partida ha finalizado en empate");
         }
